c9e87e155a6ef9064d440b86aea26a957a57f646
waco001 <abhishek.gorti@gmail.com>
2014-02-03 10:56:08 -0500

    Added main class

---
+++ simpleplatformer/Simple Platformer/src/com/gledx/simpleplatformer/Main.java
@@ -0,0 +1,8 @@
+package com.gledx.simpleplatformer;
+
+public class Main {
+	public static void main(String args[]){
+		System.setProperty("org.lwjgl.librarypath",System.getProperty("user.dir") + "/lib/");
+		System.out.println("FOO");
+	}
+}
